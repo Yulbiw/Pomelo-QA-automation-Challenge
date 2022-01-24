d2ab463e4b8dadbdd73effca1f4442f38e12bc0a
Yulbiw <wachirayana.wan@student.mahidol.ac.th>
2022-01-24 18:01:17 +0700

    Create adjusting quantity testcase

---
+++ part2/cypress/integration/shoppingbag_test.js
@@ -0,0 +1,20 @@
+describe('Shopping bag test', () => {
+    
+    beforeEach(() => {
+        cy.visit('https://www.pomelofashion.com/th/en/');
+        cy.get('button > span').contains('Shop').click();
+        cy.get('a').contains('Work Dresses').click();
+        cy.get('.product-list .product-item .product-hover-interactions__add-to-bag .option-items-wrapper .option-item.button')
+            .eq(0)
+            .click({force: true});
+        cy.get('[data-cy="cart__view_bag"]').click();
+    })
+
+    it('Adjusting product quantity', () => {
+        cy.get('.cart-item-info__quantity select').eq(0).select('2');
+        cy.wait(4000);
+        cy.get('.cart-item-info__quantity select').eq(0).should('have.value', '2');
+
+        cy.get('.cart-remove').click();
+    });
+});
